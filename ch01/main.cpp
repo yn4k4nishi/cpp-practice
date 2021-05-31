6d8ccdd842b69fbf268c713d1c0c4d6e6f0423ea
yn4k4nishi <yn4k4nishi.kit@gmail.com>
2021-05-31 20:29:02 +0900

    hello world

---
+++ ch01/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+
+    std::cout << "Hello world" << std::endl;
+
+    std::string name = "Yamada Tarou";
+    int age = 19;
+
+    std::cout << "My name is " << name << std::endl;
+    std::cout << "I am " << age << " years old." << std::endl;
+
+    return 0;
+}
